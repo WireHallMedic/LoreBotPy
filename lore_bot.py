079df53349f9cc11a27c7e6a5790a0e91fe13b62
Michael Widler <MichaelWidler@Yahoo.com>
2020-07-28 10:03:32 -0700

    add lore_bot.py with minimal functionality

---
+++ lore_bot.py
@@ -0,0 +1,42 @@
+import discord
+import json
+import re
+import random
+
+
+# let's load some things from files
+token = open("token.txt", "r").read()
+msgDict = json.loads(open("messages.json","r").read())
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+    
+@client.event
+async def on_message(message):
+   # don't respond to self, empty messages, or things that don't start with a bang
+   if message.author == client.user or \
+      len(message.content) == 0 or \
+      message.content[0] != "!":
+      return
+   
+   # we've got a potential command, format it
+   cmd = cleanMessage(message.content)
+   outStr = None
+   
+   # bot info and ToC
+   if cmd == "lorebot":
+      outStr = msgDict["botInfo"]
+      for key in msgDict["recognizedCommands"]:
+         outStr += "\n  " + key + " " + msgDict["recognizedCommands"][key]
+   
+   if outStr != None:
+      await message.channel.send(outStr)
+
+def roll(val):
+   return random.randint(1, val)
+
+# fire this bad boy up
+client.run(token)

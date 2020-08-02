dea83f36714beff335f4062c970137df47c1000d
Michael Widler <MichaelWidler@Yahoo.com>
2020-08-02 12:42:28 -0700

    add tracking for profanity

--- lore_bot.py
+++ lore_bot.py
@@ -4,6 +4,9 @@ import re
 import random
 import time
 
+notYetImplementedStr = ":warning: This feature is not yet implemented :warning:"
+profanityChirp = []
+lastSwear = 0
 
 # let's load some things from files
 token = open("token.txt", "r").read()
@@ -16,10 +19,7 @@ standardsDict = json.loads(open("standards.json","r").read())
 timeDict = json.loads(open("time.json","r").read())
 profanityDict = json.loads(open("profanity.json","r").read())
 stateDict = json.loads(open("worldstate.json","r").read())
-
-notYetImplementedStr = ":warning: This feature is not yet implemented :warning:"
-profanityChirp = []
-lastSwear = 0
+swearCountDict = json.loads(open("swearcount.json","r").read())
 
 def initBot():
    for key in profanityDict["responses"]:
@@ -58,7 +58,11 @@ async def on_message(message):
          outStr = msgDict["chirp"]["_lorebot_thinks_not_m"]
    for key in profanityDict["naughty words"]:
       if re.search(key, cmd) != None:
-         outStr = getProfanityResponse()
+         outStr = updateProfanityCount(authorName, re.search("fuck", cmd) != None)
+         if outStr == None:
+            outStr = getProfanityResponse()
+   if re.search("swear count", cmd) != None:
+      outStr = getSwearCount(authorName)
    if outStr is not None:
       outStr = outStr.replace("[NAME]", authorName)
       outStr = outStr.replace("[BAD_ODDS]", str(roll(50) + 50))
@@ -144,6 +148,37 @@ def getProfanityResponse():
       return None
    lastSwear = curTime
    return random.choice(profanityChirp)
+
+# update profanity count
+def updateProfanityCount(author, isFuck):
+   authorTotalStr = "{}_t".format(author)
+   authorFBombStr = "{}_f".format(author)
+   if authorTotalStr in swearCountDict:
+      swearCountDict[authorTotalStr] = swearCountDict[authorTotalStr] + 1
+   else:
+      swearCountDict[authorTotalStr] = 1
+   if authorFBombStr not in swearCountDict:
+      swearCountDict[authorFBombStr] = 0
+   if isFuck:
+      swearCountDict[authorFBombStr] = swearCountDict[authorFBombStr] + 1
+   # save current state
+   with open('swearcount.json', 'w') as json_file:
+      json.dump(swearCountDict, json_file)
+   if swearCountDict[authorTotalStr] == 10:
+      return ":trophy:Achievement Unlocked: Potty Mouth!\n[NAME] has sworn 10 times!\nEnter command '!swear count' to see your stats!"
+   if swearCountDict[authorTotalStr] == 100:
+      return ":trophy:Achievement Unlocked: Advanced Potty Mouth!\n[NAME] has sworn 100 times!\nEnter command '!swear count' to see your stats!"
+   if swearCountDict[authorTotalStr] == 1000:
+      return ":trophy:Achievement Unlocked: Master Potty Mouth!\n[NAME] has sworn 1000 times!\nEnter command '!swear count' to see your stats!"
+   return None
+   
+def getSwearCount(author):
+   authorTotalStr = "{}_t".format(author)
+   authorFBombStr = "{}_f".format(author)
+   if authorTotalStr in swearCountDict:
+      return "Since I began counting, user [NAME] has sworn {} times, of which {} were f-bombs.".format( \
+         swearCountDict[authorTotalStr], swearCountDict[authorFBombStr])
+   return "I have no swearing on record for user [NAME]."
 
 # fire this bad boy up
 client.run(token)

2b4f881e7a577a3ff294de90bdee0324c4321179
WireHallMedic <michaelwidler@yahoo.com>
2022-09-19 08:30:38 -0700

    Add intents

--- lore_bot.py
+++ lore_bot.py
@@ -40,7 +40,10 @@ def initBot():
 
 initBot()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
